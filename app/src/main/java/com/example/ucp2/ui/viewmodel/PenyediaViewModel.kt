6df76cddbe97a1ebc2fddd65846e7f45eb74bf5b
Azmi <azmianshari123@gmail.com>
2024-12-20 21:22:05 +0700

    Menambahkan import KrsApp dan memperbaiki beberapa kode

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -3,24 +3,25 @@ package com.example.ucp2.ui.viewmodel
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KrsApp
 import com.example.ucp2.repository.LocalRepositoryDsn
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             DosenViewModel(
-                KrsApp().containerApp.repositoryDosen as LocalRepositoryDsn
+                KrsApp().containerApp.RepositoryDosen as LocalRepositoryDsn
             )
         }
         initializer {
             HomeDosenViewModel(
-                KrsApp().containerApp.repositoryDosen
+                KrsApp().containerApp.RepositoryDosen
             )
         }
         initializer {
             DetailDosenViewModel(
                 createSavedStateHandle(),
-                KrsApp().containerApp.repositoryDosen
+                KrsApp().containerApp.RepositoryDosen
             )
         }
     }
